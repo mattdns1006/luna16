fadd1c9f6857ed159697fae66645908fc1160fc8
snow_leps <mattdns100689@gmail.com>
2016-03-11 17:05:44 +0000

    Added parrallel network structure

--- models.lua
+++ models.lua
@@ -1,9 +1,12 @@
 modelLayers = require "modelLayers"
 
+local size = 38
+eg = torch.range(1,torch.pow(size,3),1):reshape(1,1,size,size,size)
+
 models = {}
 
 -- 64^3
-function models.model1()
+function models.model5()
 	nFiltersInc = 96 
 	nFilters = {1,nFiltersInc,nFiltersInc*2,nFiltersInc*3,nFiltersInc*4}
 	filterSizeConv = {3,3,3,3,3}
@@ -39,39 +42,63 @@ function models.model1()
 end
 
 
-function models.parallelNetwork()
-
-	nFiltersInc = 64 
-	Filters = {1,nFiltersInc,nFiltersInc*2,nFiltersInc*3,nFiltersInc*4}
+function miniNetwork()
+	local model = nn.Sequential()
+	nFiltersInc = 96 
+	nFilters = {1,nFiltersInc,nFiltersInc*2,nFiltersInc*3,nFiltersInc*4}
 	filterSizeConv = {3,3,3,3,3}
 	strideConv = {1,1,1,1,1}
 	paddingConv = {1,1,1,1,1}
 
-	filterSizeConv = {5,5,5,5,5}
-	strideConv = {1,1,1,1,1}
-	paddingConv = {2,2,2,2,2}
-
 	sizeMP = {3,3,3,3,3}
 	strideMP = {2,2,2,2,2}
-	addingMP = {1,1,1,1,1}
+	paddingMP = {1,1,1,1,1}
 
 	model = nn.Sequential()
-	c = nn.ConcatTable()
-	modelLayers.add3DConv(c, layerNu, nFilters, filterSizeConv, strideConv, paddingConv)
-	modelLayers.add3DConv(c, layerNu, nFilters, filterSizeConv, strideConv, paddingConv)
 
-	p1 = nn.ParallelTable()
-	p1:add(nn.Tanh())
-	p1:add(nn.Tanh())
-	model:add(p1)
-	
-	p2 = nn.ParallelTable()
-	modelLayers.addMP(model, layerNu, sizeMP, strideMP, paddingMP)
-	modelLayers.addMP(model, layerNu, sizeMP, strideMP, paddingMP)
-	model:add(p2)
-	
-	model:add(nn.JoinTable(2))
+	layerNu = 2 
+	modelLayers.addBN(model, 1, 1)
+	--model:add(nn.VolumetricConvolution(1,1,2,2,2,1,1,1,0,0,0))
+	for i = 1,3 do 
+
+		modelLayers.add3DConv(model, layerNu, nFilters, filterSizeConv, strideConv, paddingConv)
+		--model:add(nn.ReLU())
+		--model:add(nn.PReLU())
+		model:add(nn.Tanh())
+		modelLayers.addMP(model, layerNu, sizeMP, strideMP, paddingMP)
+		modelLayers.addBN(model, layerNu, nFilters)
+		layerNu = layerNu + 1
+	end
+	lastLayerNeurons = nFiltersInc*3*3*3*3
+	model:add(nn.View(lastLayerNeurons))
+	--model:add(nn.Linear(lastLayerNeurons,lastLayerNeurons))
+	model:add(nn.Linear(lastLayerNeurons,1))
 	return model
+end
+
+function models.parallelNetwork()
+
+	-- Seperate networks all take same input size
+	--- |  |  |
+	--  \  |  /
+	--   \ | /
+	--     |   (concat)
+	--   output 
+	
+	-- Seperate (mini) network architecture
+	-- Parallel Table forwards the ith member module to the i-th input i.e. we need to feed it a table of size 3 in this case
+	model = nn.Sequential()
+
+	mother = nn.ParallelTable(3)	
+	mother:add(miniNetwork())	
+	mother:add(miniNetwork())	
+	mother:add(miniNetwork())	
+	model:add(mother)
+	model:add(nn.JoinTable(1))
+	model:add(nn.Linear(3,1))
+	model:add(nn.Sigmoid())
+
+	return model 
 
 end
 return models
