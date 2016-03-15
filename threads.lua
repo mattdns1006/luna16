63252258258eb3b0d86f4b019094c8b30ef8550f
snow_leps <mattdns100689@gmail.com>
2016-03-15 13:39:06 +0000

    oo version of threads - do not understand

--- threads.lua
+++ threads.lua
@@ -1,10 +1,58 @@
-
 require "cunn"
-threads = require 'threads'
-threads.serialization('threads.sharedserialize')
-nthread = 4 
-njob = 5 
+Threads = require 'threads'
+Threads.serialization('threads.sharedserialize')
 
+local data = {}
+local result = {}
+local unpack = unpack and unpack or table.unpack
+
+function data.new(nThreads)
+	local self = {}
+	self.threads = Threads(nThreads,
+			   function()
+				   loadData = require "loadData"
+				   loadData.Init()
+		           end
+			)
+	self.threads:synchronize()
+	for i=1, nThreads do
+		self.threads:addjob(self._getFromThreads,
+				    self._pushResult)
+	end
+	return self
+end
+
+function data._getFromThreads()
+	      x,y = loadData.getBatch(C0,C1,1,36,-1200,1200,0.4,0.3,0,params.para)
+	      return x,y
+end
+
+function data._pushResult(...)
+	local res = {...}
+	if res == nil then 
+		self.threads:synchronize()
+	end
+	result[1] = res
+end
+
+function data:getBatch()
+	self.threads:addjob(self._getFromThreads, self._pushResult)
+	self.threads:dojob()
+	local res = result[1]
+		result[1] = nil
+	if torch.type(res) == 'table' then
+		return unpack(res)
+	end
+		print(type(res))
+	return res
+end
+
+d = data.new(2)
+
+
+
+
+--[[
 Queue = require 'threads.queue'
 
 donkeys = threads.Threads(
@@ -29,4 +77,5 @@ for i=1, 10 do
 			end)
 end
 donkeys:synchronize()
+]]--
 
