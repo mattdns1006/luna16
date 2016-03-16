a48ebce3f1cf72cc95acc6572b57cc6232a23270
snow_leps <mattdns100689@gmail.com>
2016-03-16 09:29:15 +0000

    Fixed oo version of threads but still slower

--- threads.lua
+++ threads.lua
@@ -1,43 +1,85 @@
 require "cunn"
 Threads = require 'threads'
 Threads.serialization('threads.sharedserialize')
-nThreads = 6 
-opt = {}
-do
-	local options = opt -- make an upvalue to serialize over to donkey threads
-	donkeys = Threads(
-		nThreads,
-		function()
-			require 'torch'
-		end,
-		function(idx)
-			opt = options -- pass to all donkeys via upvalue
-			loadData = require "loadData"
-			loadData.Init()
-			print("initialization")
-		end
-		)
-end
 
-X = {}
-Y = {}
-donkeys:synchronize()
+DataThreads = {}
+DataThreads.__index = DataThreads
 
-time = torch.Timer()
-time2 = torch.Timer()
-for i = 1, 100 do
-	donkeys:addjob(function()
-				x,y = loadData.getBatch(C0,C1,1,36,-1200,1200,0.4,0.3,0,params.para)
-				--x,y = torch.random(), torch.uniform()
-				return x,y
-			end,
-			function(x,y)
-				X = x
-				Y = y
-			end
+local result = {}
+local unpack = unpack and unpack or table.unpack
+
+function DataThreads.new(nThreads,opt_)
+	local self = setmetatable({}, DataThreads)
+	opt_ = opt_ or {}
+	self.threads = Threads(nThreads,
+			   function()
+				   print("background stuff")
+				   loadData = require "loadData"
+				   loadData.Init()
+				   print("done")
+		           end
 			)
-	print(Y)
-	print(time:time().real)
-	time:reset()
+
+	for i=1, nThreads do
+		self.threads:addjob(self._getFromThreads, self._pushResult)
+	end
+	self.threads:synchronize()
+	return self
 end
-print(time2:time().real)
+
+
+function DataThreads._getFromThreads()
+	      x,y = loadData.getBatch(C0,C1,1,36,-1200,1200,0.4,0.3,0,params.para)
+	      --x,y = torch.uniform(),torch.uniform()
+	      return x,y
+end
+
+function DataThreads._pushResult(...)
+	local res = {...}
+	if res == nil then 
+		self.threads:synchronize()
+	end
+	result[1] = res
+end
+
+
+function DataThreads:getBatch()
+	self.threads:addjob(self._getFromThreads, self._pushResult)
+	self.threads:dojob()
+	local res = result[1]
+		result[1] = nil
+	if torch.type(res) == 'table' then
+		return unpack(res)
+	end
+		print(type(res))
+	return res
+end
+local n = 30
+timer = torch.Timer()
+overallTime = torch.Timer()
+
+--[[
+d = DataThreads.new(4)
+
+
+
+
+for i = 1, n do
+	x,y = d:getBatch()
+	print(timer:time().real)
+	timer:reset()
+end
+print("Overall time for "..n.." = " .. overallTime:time().real)
+]]--
+
+
+loadData = require "loadData"
+loadData.Init()
+overallTime:reset()
+for i = 1, n do
+     x,y = loadData.getBatch(C0,C1,1,36,-1200,1200,0.4,0.3,0,params.para)
+     print(timer:time().real)
+     timer:reset()
+end
+print("Overall time for "..n.." = " .. overallTime:time().real)
+
